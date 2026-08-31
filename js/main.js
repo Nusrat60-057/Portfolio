79dc2b0e527b8b1d3262f10f3e62b4db496e6a1d
Nusrat Rahman Aurna <2023-3-60-057@std.ewubd.edu>
2026-08-31 10:32:54 +0600

    Updated the UI and fixed the white line issue

--- js/main.js
+++ js/main.js
@@ -11,42 +11,47 @@
     };
     spinner(0);
     
-    
     // Initiate the wowjs
-    new WOW().init();
-
-
-
-   // testimonial carousel
-   $(".testimonial-carousel").owlCarousel({
-    autoplay: true,
-    items: 1,
-    smartSpeed: 1500,
-    dots: true,
-    dotsData: true,
-    loop: true,
-    margin: 25,
-    nav : true,
-    navText : [
-        '<i class="bi bi-arrow-left"></i>',
-        '<i class="bi bi-arrow-right"></i>'
-    ]
-    });
-
-
-   // Back to top button
-   $(window).scroll(function () {
-    if ($(this).scrollTop() > 300) {
-        $('.back-to-top').fadeIn('slow');
-    } else {
-        $('.back-to-top').fadeOut('slow');
+    if (typeof WOW !== 'undefined') {
+        new WOW().init();
     }
+
+    // Smooth scroll for nav links & back to top
+    $('.navbar-nav a.nav-link[href^="#"], a.btn[href^="#"]').on('click', function (e) {
+        var target = $(this.getAttribute('href'));
+        if (target.length) {
+            e.preventDefault();
+            $('html, body').stop().animate({
+                scrollTop: target.offset().top - 20
+            }, 800);
+        }
     });
+
+    // Active link highlighting on scroll
+    $(window).on('scroll', function () {
+        var scrollPos = $(window).scrollTop() + 100;
+        $('.navbar-nav a.nav-link').each(function () {
+            var currLink = $(this);
+            var refElement = $(currLink.attr("href"));
+            if (refElement.length) {
+                if (refElement.position().top <= scrollPos && refElement.position().top + refElement.height() > scrollPos) {
+                    $('.navbar-nav a.nav-link').removeClass("active");
+                    currLink.addClass("active");
+                }
+            }
+        });
+
+        // Back to top button visibility
+        if ($(this).scrollTop() > 300) {
+            $('.back-to-top').fadeIn('slow');
+        } else {
+            $('.back-to-top').fadeOut('slow');
+        }
+    });
+
     $('.back-to-top').click(function () {
-        $('html, body').animate({scrollTop: 0}, 1500, 'easeInOutExpo');
+        $('html, body').animate({scrollTop: 0}, 800);
         return false;
     });
 
-
 })(jQuery);
-
